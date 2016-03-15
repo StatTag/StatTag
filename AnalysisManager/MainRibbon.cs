2a05bcce1fa480b309e53d2e3f358596c8fba4c8
Luke Rasmussen <luke.rasmussen@gmail.com>
2016-03-15 14:14:11 -0500

    Closes NUCATSAM114 Refresh code on document open

--- AnalysisManager/MainRibbon.cs
+++ AnalysisManager/MainRibbon.cs
@@ -31,6 +31,11 @@ namespace AnalysisManager
             get { return Globals.ThisAddIn.LogManager; }
         }
 
+        public StatsManager StatsManager
+        {
+            get { return Globals.ThisAddIn.StatsManager; }
+        }
+
         private void MainRibbon_Load(object sender, RibbonUIEventArgs e)
         {
 
@@ -87,7 +92,7 @@ namespace AnalysisManager
                     {
                         if (!refreshedFiles.Contains(annotation.CodeFile))
                         {
-                            var result = ExecuteStatPackage(annotation.CodeFile,
+                            var result = StatsManager.ExecuteStatPackage(annotation.CodeFile,
                                 Constants.ParserFilterMode.AnnotationList, annotations);
                             if (!result.Success)
                             {
@@ -121,96 +126,6 @@ namespace AnalysisManager
                     Cursor.Current = Cursors.Default;   
                 }
             }
-        }
-
-        /// <summary>
-        /// Used exclusively by ExecuteStatPackage as its return value type.
-        /// </summary>
-        private class ExecuteResult
-        {
-            /// <summary>
-            /// Did the call to ExecuteStatPackage complete without any errors
-            /// </summary>
-            public bool Success { get; set; }
-
-            /// <summary>
-            /// A list of Annotations that were detected as having changed values since they
-            /// were originally inserted into the document
-            /// </summary>
-            public List<Annotation> UpdatedAnnotations { get; set; } 
-        }
-
-        /// <summary>
-        /// Run the statistical package for a given code file.
-        /// </summary>
-        /// <param name="file">The code file to execute</param>
-        /// <param name="filterMode">The type of filter to apply on the types of commands to execute</param>
-        /// <param name="annotationsToRun">An optional list of annotations to execute code for.  This is only applied when the filter mode is ParserFilterMode.AnnotationList</param>
-        /// <returns></returns>
-        private ExecuteResult ExecuteStatPackage(CodeFile file, int filterMode = Constants.ParserFilterMode.ExcludeOnDemand, List<Annotation> annotationsToRun = null)
-        {
-            var result = new ExecuteResult() {Success = false, UpdatedAnnotations = new List<Annotation>()};
-            var automation = new Stata.Automation();
-            if (!automation.Initialize())
-            {
-                MessageBox.Show(
-                    "Could not communicate with Stata.  You will need to enable Stata Automation (not done by default) to run this code in Analysis Manager.\r\n\r\nThis can be done from Analysis Manager > Settings, or see http://www.stata.com/automation",
-                    UIUtility.GetAddInName());
-                return result;
-            }
-
-            var parser = Factories.GetParser(file);
-            if (parser == null)
-            {
-                return result;
-            }
-
-            try
-            {
-                // Get all of the commands in the code file that should be executed given the current filter
-                var steps = parser.GetExecutionSteps(file.LoadFileContent(), filterMode, annotationsToRun);
-                foreach (var step in steps)
-                {
-                    var results = automation.RunCommands(step.Code.ToArray());
-                    if (step.Annotation == null)
-                    {
-                        continue;
-                    }
-
-                    var annotation = Manager.FindAnnotation(step.Annotation.Id);
-                    if (annotation != null)
-                    {
-                        var resultList = new List<CommandResult>(results);
-
-                        // Determine if we had a cached list, and if so if the results have changed.
-                        bool resultsChanged = (annotation.CachedResult != null &&
-                                                !resultList.SequenceEqual(annotation.CachedResult));
-                        annotation.CachedResult = resultList;
-
-                        // If the results did change, we need to sweep the document and update all of the results
-                        if (resultsChanged)
-                        {
-                            // For all table annotations, update the formatted cells collection
-                            if (annotation.IsTableAnnotation())
-                            {
-                                annotation.CachedResult.FindAll(x => x.TableResult != null).ForEach(
-                                    x => x.TableResult.FormattedCells = annotation.TableFormat.Format(x.TableResult));
-                            }
-
-                            result.UpdatedAnnotations.Add(annotation);
-                        }
-                    }
-                }
-
-                result.Success = true;
-            }
-            catch (Exception exc)
-            {
-                MessageBox.Show(exc.Message, UIUtility.GetAddInName(), MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return result;
-            }
-
-            return result;
         }
 
         private void cmdSettings_Click(object sender, RibbonControlEventArgs e)
@@ -252,7 +167,7 @@ namespace AnalysisManager
                 {
                     if (!refreshedFiles.Contains(codeFile))
                     {
-                        var result = ExecuteStatPackage(codeFile, Constants.ParserFilterMode.AnnotationList, annotations);
+                        var result = StatsManager.ExecuteStatPackage(codeFile, Constants.ParserFilterMode.AnnotationList, annotations);
                         if (!result.Success)
                         {
                             break;
