3704cc32f72ee6d7b9d3dd8e9777203b8f5b8b6d
Luke Rasmussen <luke.rasmussen@gmail.com>
2016-03-29 17:22:39 -0500

    Closes NUCATSAM-120 Display calculations

--- Stata/Automation.cs
+++ Stata/Automation.cs
@@ -15,6 +15,11 @@ namespace Stata
     {
         public const string LocalMacroPrefix = "_";
 
+        /// <summary>
+        /// This is a special local macro name that is being used within Analysis Manager.
+        /// </summary>
+        public const string AnalysisManagerTempMacroName = "__am_tmp_display_value";
+
         // The following are constants used to manage the Stata Automation API
         public const string RegisterParameter = "/Register";
         public const string UnregisterParameter = "/Unregister";
@@ -25,7 +30,7 @@ namespace Stata
         /// <summary>
         /// The collection of all possible Stata process names.  These are converted to
         /// lower case here because the comparison we do later depends on conversion to
-        /// lower case.s
+        /// lower case.
         /// </summary>
         private static readonly List<string> StataProcessNames = new List<string>(new []
         {
@@ -107,6 +112,20 @@ namespace Stata
         public string GetDisplayResult(string command)
         {
             string name;
+
+            // Display values with calculations in them require special handling.  The API does not
+            // process them directly, so our workaround is to introduce a local macro to process the
+            // calculcation, and then use the downstream macro result handler to pull out the result.
+            // This will work even if the same local macro is defined multiple times in the same
+            // execution.
+            if (Parser.IsCalculatedDisplayValue(command))
+            {
+                name = Parser.GetValueName(command);
+                command = string.Format("local {0} = {1}", AnalysisManagerTempMacroName, name);
+                RunCommand(command);
+                command = string.Format("display `{0}'", AnalysisManagerTempMacroName);
+            }
+
             if (Parser.IsMacroDisplayValue(command))
             {
                 name = Parser.GetMacroValueName(command);
