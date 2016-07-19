e7b1c26ac0e7cc9480701cb74087234230f6e2c2
Luke Rasmussen <luke.rasmussen@gmail.com>
2016-07-19 14:43:48 -0500

    Updated About dialog and user guide

--- StatTag/About.Designer.cs
+++ StatTag/About.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            System.Windows.Forms.TextBox textBox1;
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -37,6 +38,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.lblCopyright = new System.Windows.Forms.Label();
             this.lblVersion = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmdOK = new System.Windows.Forms.Button();
+            textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,9 +53,9 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(15, 59);
+            this.groupBox1.Location = new System.Drawing.Point(15, 137);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(583, 266);
+            this.groupBox1.Size = new System.Drawing.Size(636, 266);
             this.groupBox1.TabIndex = 15;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Acknowledgements";
@@ -96,7 +100,7 @@
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(6, 101);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(570, 48);
+            this.label2.Size = new System.Drawing.Size(623, 48);
             this.label2.TabIndex = 1;
             this.label2.Text = resources.GetString("label2.Text");
             // 
@@ -107,33 +111,71 @@
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(7, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(570, 64);
+            this.label1.Size = new System.Drawing.Size(623, 64);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
             // lblCopyright
             // 
             this.lblCopyright.AutoSize = true;
-            this.lblCopyright.Location = new System.Drawing.Point(12, 26);
+            this.lblCopyright.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright.Location = new System.Drawing.Point(12, 29);
             this.lblCopyright.Name = "lblCopyright";
-            this.lblCopyright.Size = new System.Drawing.Size(54, 17);
+            this.lblCopyright.Size = new System.Drawing.Size(62, 20);
             this.lblCopyright.TabIndex = 17;
             this.lblCopyright.Text = "(c) 2016";
             // 
             // lblVersion
             // 
             this.lblVersion.AutoSize = true;
+            this.lblVersion.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblVersion.Location = new System.Drawing.Point(12, 9);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(79, 17);
+            this.lblVersion.Size = new System.Drawing.Size(90, 20);
             this.lblVersion.TabIndex = 16;
             this.lblVersion.Text = "StatTag v1.0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Please cite StatTag as:";
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.cmdOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.cmdOK.Location = new System.Drawing.Point(287, 428);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(87, 25);
+            this.cmdOK.TabIndex = 19;
+            this.cmdOK.Text = "Close";
+            this.cmdOK.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            textBox1.CausesValidation = false;
+            textBox1.Location = new System.Drawing.Point(28, 82);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new System.Drawing.Size(623, 41);
+            textBox1.TabIndex = 21;
+            textBox1.Text = "Welty, L.J., Rasmussen, L.V., & Baldridge, A.S. (2016). StatTag. Chicago, Illinoi" +
+    "s, United States: Galter Health Sciences Library. doi:10.18131/G3K76";
             // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 337);
+            this.ClientSize = new System.Drawing.Size(660, 461);
+            this.Controls.Add(textBox1);
+            this.Controls.Add(this.cmdOK);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.lblCopyright);
             this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.groupBox1);
@@ -160,5 +202,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button cmdOK;
     }
 }
