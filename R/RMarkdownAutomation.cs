132eb3e1e01f7b18aea966e4f0ec01ed457245b8
Luke Rasmussen <luke.rasmussen@gmail.com>
2018-12-07 11:36:49 -0600

    Closes NUCATSAM-614 Detect if knitr is installed

--- R/RMarkdownAutomation.cs
+++ R/RMarkdownAutomation.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RDotNet;
+using StatTag.Core.Exceptions;
 using StatTag.Core.Models;
 using StatTag.Core.Parser;
 using StatTag.Core.Utility;
@@ -17,6 +18,35 @@ namespace R
         {
             Parser = new RMarkdownParser();
             State = new StatPackageState();
+        }
+
+        public override bool Initialize(CodeFile file, LogManager logger)
+        {
+            // Let the base initialization run so we know we have a valid R engine
+            if (!base.Initialize(file, logger))
+            {
+                return false;
+            }
+
+            // We require that the knitr package be installed if a user wants to run R Markdown.
+            // This is because we use the purl() command from knitr to extract the R code.
+            // We think this is a fair assumption because the recommendation from StatTag is to
+            // run your code to completion before running it in StatTag.  That means the user
+            // should have knitted their R Markdown document.
+            var expression = Engine.Evaluate("require('knitr')");
+            if (expression != null)
+            {
+                var result = expression.AsLogical();
+                if (result != null && result.Length > 0)
+                {
+                    if (result[0])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            throw new StatTagUserException("To run R Markdown documents, StatTag requires that you have the knitr package installed.\r\n\r\nPlease see the User’s Guide for more information.");
         }
     }
 }
