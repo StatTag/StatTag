448a76d9f7f0d15f3767e24ac35e3d2a8e239e1e
Luke Rasmussen <luke.rasmussen@gmail.com>
2019-08-22 11:48:06 -0500

    Improve testing around Python images

--- Core/Parser/PythonParser.cs
+++ Core/Parser/PythonParser.cs
@@ -12,7 +12,14 @@ namespace StatTag.Core.Parser
 {
     public class PythonParser : BaseParser
     {
-        private static readonly Regex FigureRegex = new Regex(string.Format(".+\\((\\s*?[\\s\\S]*)\\)"));
+        /// <summary>
+        /// Identifies a string that represents the path to a figure/image
+        /// </summary>
+        /// <remarks>Because Python has several plotting libraries, we are not going to try and identify
+        /// a set of built-in method names to detect that it is a figure.  It will be up to the user
+        /// to tag correctly, and for us to handle errors correctly.  Instead, we're just going to detect
+        /// anything within parentheses as valid.</remarks>
+        private static readonly Regex FigureRegex = new Regex(string.Format(".+?\\((\\s*?[\\s\\S]+)\\)"));
 
         public override string CommentCharacter
         {
@@ -21,19 +28,35 @@ namespace StatTag.Core.Parser
 
         public override bool IsImageExport(string command)
         {
-            // We consider any tagged command a candidate for an image result
-            return true;
+            // If the command is blank/null, there's no way it's an image export command.
+            if (string.IsNullOrWhiteSpace(command))
+            {
+                return false;
+            }
+
+            return FigureRegex.IsMatch(command);
         }
 
+        /// <summary>
+        /// Return the variable name or file path used to save an image
+        /// </summary>
+        /// <param name="command">The command to extract the image save path from</param>
+        /// <returns>A string containing the Python expression to define the image save location, or an empty string if one cannot be found.</returns>
         public override string GetImageSaveLocation(string command)
         {
+            // If the command is blank/null, return our default value of a blank string.
+            if (string.IsNullOrWhiteSpace(command))
+            {
+                return string.Empty;
+            }
+
             var match = FigureRegex.Match(command);
             if (!match.Success || match.Groups.Count < 2)
             {
                 return string.Empty;
             }
 
-            return match.Groups[1].Value;
+            return match.Groups[1].Value.Trim();
         }
 
         public override bool IsValueDisplay(string command)
