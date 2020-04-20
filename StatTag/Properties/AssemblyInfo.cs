69e697d6ab3b0a584b19c1dbafa950d625542bb8
Luke Rasmussen <luke.rasmussen@gmail.com>
2020-04-20 10:11:56 -0500

    v6.0.0 release

--- StatTag/Properties/AssemblyInfo.cs
+++ StatTag/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Security;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Northwestern University, Feinberg School of Medicine")]
 [assembly: AssemblyProduct("StatTag")]
-[assembly: AssemblyCopyright("Copyright © 2016-2019")]
+[assembly: AssemblyCopyright("Copyright © 2016-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.0.0.4")]
-[assembly: AssemblyFileVersion("6.0.0-beta5")]
+[assembly: AssemblyVersion("6.0.0.5")]
+[assembly: AssemblyFileVersion("6.0.0")]
