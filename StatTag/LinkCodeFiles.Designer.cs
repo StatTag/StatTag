72a7d711cf54c7aade1483aec6794364d7a29570
Luke Rasmussen <luke.rasmussen@gmail.com>
2022-07-06 08:18:26 -0500

    Closes #24 - Link Code Files long paths easier to read
    Set up the cells to support wrapping to make it easier to read long file
    paths without having to resize the dialog.

--- StatTag/LinkCodeFiles.Designer.cs
+++ StatTag/LinkCodeFiles.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LinkCodeFiles));
             System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LinkCodeFiles));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.dgvCodeFiles = new System.Windows.Forms.DataGridView();
             this.colMissingCodeFile = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -37,9 +38,30 @@
             this.cmdCancel = new System.Windows.Forms.Button();
             this.cmdOK = new System.Windows.Forms.Button();
             tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCodeFiles)).BeginInit();
             tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCodeFiles)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(this.cmdCancel, 1, 2);
+            tableLayoutPanel1.Controls.Add(this.dgvCodeFiles, 0, 1);
+            tableLayoutPanel1.Controls.Add(this.cmdOK, 0, 2);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.Size = new System.Drawing.Size(776, 383);
+            tableLayoutPanel1.TabIndex = 8;
             // 
             // label1
             // 
@@ -57,12 +79,21 @@
             this.dgvCodeFiles.AllowUserToAddRows = false;
             this.dgvCodeFiles.AllowUserToDeleteRows = false;
             this.dgvCodeFiles.AllowUserToResizeRows = false;
+            this.dgvCodeFiles.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
             this.dgvCodeFiles.CausesValidation = false;
             this.dgvCodeFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvCodeFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colMissingCodeFile,
             this.colActionToTake});
             tableLayoutPanel1.SetColumnSpan(this.dgvCodeFiles, 2);
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvCodeFiles.DefaultCellStyle = dataGridViewCellStyle1;
             this.dgvCodeFiles.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvCodeFiles.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
             this.dgvCodeFiles.Location = new System.Drawing.Point(6, 74);
@@ -120,27 +151,6 @@
             this.cmdOK.UseVisualStyleBackColor = true;
             this.cmdOK.Click += new System.EventHandler(this.cmdOK_Click);
             // 
-            // tableLayoutPanel1
-            // 
-            tableLayoutPanel1.AutoSize = true;
-            tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            tableLayoutPanel1.Controls.Add(this.cmdCancel, 1, 2);
-            tableLayoutPanel1.Controls.Add(this.dgvCodeFiles, 0, 1);
-            tableLayoutPanel1.Controls.Add(this.cmdOK, 0, 2);
-            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
-            tableLayoutPanel1.RowCount = 3;
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            tableLayoutPanel1.Size = new System.Drawing.Size(776, 383);
-            tableLayoutPanel1.TabIndex = 8;
-            // 
             // LinkCodeFiles
             // 
             this.AcceptButton = this.cmdOK;
@@ -156,9 +166,9 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Link Code Files";
             this.Load += new System.EventHandler(this.LinkCodeFiles_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCodeFiles)).EndInit();
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCodeFiles)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
